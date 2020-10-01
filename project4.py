b73afcd516c391ad0f9dc8c41fa7c4ce732b7426
abrarsim2017 <abrarsim2017@gmail.com>
2020-09-30 21:30:41 -0500

    Count files in months and days

--- project4.py
+++ project4.py
@@ -19,6 +19,26 @@ error_count = 0
 unsuc_percent = 0
 redir_percent = 0
 dates = {}
-months ={}
+months = {}
+files = {}
 
 for line in lines:
+  line = line.split(" ")
+  a = " "
+  if len(line) == 9:
+    if len(line[3]) >= 19:
+      a = line[3].replace('[', '')
+      a = a[0:11]
+      date_f = datetime.datetime.strptime(a, '%d/%b/%Y').date()
+      month_f = date_f.month
+      
+      if a in dates:
+        dates[a] += 1
+      else:
+        dates[a] = 1
+      if month_f in months:
+        months[month_f] += 1
+      else:
+        months[month_f] = 1
+       
+      
